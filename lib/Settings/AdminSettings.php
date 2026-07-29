00eb7a4ecbf7054f83db530b917762a755638c82
zomtec2311 <wtoedt@gmail.com>
2026-07-29 13:58:30 +0200

    Add files via upload

--- lib/Settings/AdminSettings.php
+++ lib/Settings/AdminSettings.php
@@ -1,4 +1,29 @@
 <?php
+/**
+ *
+ * AppStoreSwitcher APP (Nextcloud)
+ *
+ * @author Wolfgang Tödt <wtoedt@gmail.com>
+ *
+ * @copyright Copyright (c) 2026 Wolfgang Tödt
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 declare(strict_types=1);
 
 namespace OCA\AppStoreSwitcher\Settings;
@@ -11,7 +36,7 @@ use OCP\Settings\ISettings;
 class AdminSettings implements ISettings {
     private IConfig $config;
     private IInitialState $initialState;
-    private string $appName = 'appstore_switcher'; // Wichtig: Der Name Ihrer App
+    private string $appName = 'appstore_switcher';
 
     public function __construct(IConfig $config, IInitialState $initialState) {
         $this->config = $config;
